347ab8ad684cb6f914ea0775e7421ce7032f4bbb
Angel Mthimunye <62521227+AngelMthimunye@users.noreply.github.com>
2020-07-09 19:46:10 +0200

    Add files via upload

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://localhost:5000/predict_api"
-r = requests.post(url, json={"city":1, "area":213, "rooms":2, "bathroom":1, "parking spaces":1, "floor":2, "animal":1, "furniture":1})
+r = requests.post(url, json={"city":1, "rooms":2, "bathroom":1, "parking spaces":1, "floor":2, "animal":1, "furniture":1})
 
 print(r.json())
 

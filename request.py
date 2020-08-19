e9d87bbbc909f9e3251db3ef2068fa66e05baa73
Angel Mthimunye <62521227+AngelMthimunye@users.noreply.github.com>
2020-08-19 12:02:35 +0200

    Delete request.py

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/predict_api"
-r = requests.post(url, json={"city":1, "rooms":2, "bathroom":1, "parking spaces":1, "floor":2, "animal":1, "furniture":1})
-
-print(r.json())
-
-
